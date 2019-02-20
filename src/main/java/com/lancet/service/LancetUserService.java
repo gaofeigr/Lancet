2164c934c0cf84cb7cb3814c4ac9911af758f295
Gawain <gaofeigr@outlook.com>
2019-02-20 22:19:14 +0800

    修改包名

---
+++ src/main/java/com/lancet/service/LancetUserService.java
@@ -0,0 +1,39 @@
+package com.lancet.service;
+
+import com.lancet.entity.LancetUser;
+
+/**
+ * @Description 用户Service
+ * @Date 2019-2-20 21:00
+ * @Author GaoFei
+ * @Version 1.0
+ **/
+public interface LancetUserService {
+    /**
+     * 新增
+     * @param lancetUser
+     * @return
+     */
+    void add(LancetUser lancetUser);
+
+    /**
+     * 更新
+     * @param lancetUser
+     * @return
+     */
+    void update(LancetUser lancetUser);
+
+    /**
+     * 删除
+     * @param lancetUser
+     * @return
+     */
+    void delete(LancetUser lancetUser);
+
+    /**
+     * 根据id查找
+     * @param id
+     * @return
+     */
+    LancetUser findById(String id);
+}
